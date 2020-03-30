8d59b2402734a6d072b21843465f37ddc48e1fe4
Neven Kalakovic <nk@cashsentinel.com>
2020-03-30 17:44:22 +0200

    Initial commit

---
+++ src/main/java/app/hyperdata/audit/HdAuditServiceApplication.java
@@ -0,0 +1,13 @@
+package app.hyperdata.audit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdAuditServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdAuditServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/hyperdata/audit/HdAuditServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package app.hyperdata.audit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdAuditServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
